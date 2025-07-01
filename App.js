f0fa9f753fb5bdbf2f3a1d79a46d5e81b5c8d84e
praneeth <gp.gunturu@gmail.com>
2025-07-01 13:03:13 -0500

    installed React Navigation and expo dependencies to use navigation

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
     return (
         <View>
-        <StatusBar style='light' />
-        <CategoriesScreen />
+            <StatusBar style='light' />
+            <NavigationContainer>
+                <CategoriesScreen />
+            </NavigationContainer>
         </View>
     );
 }
