be90aaa98c94369decdda55c56dc7d76ffc5688a
praneeth <gp.gunturu@gmail.com>
2025-07-01 12:07:07 -0500

    custom component to display the categories

---
+++ components/CategoryGridTitle.js
@@ -0,0 +1,13 @@
+import { Pressable, View } from "react-native";
+
+export default function CategoryGridTitle({title, color}) {
+    return (
+        <View>
+            <Pressable>
+                <View>
+                    <Text>{title}</Text>
+                </View>
+            </Pressable>
+        </View>
+    )
+}
