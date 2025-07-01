93f1318abc87b171f6571ccb22cf16fd52c44aab
praneeth <gp.gunturu@gmail.com>
2025-07-01 18:23:26 -0500

    new component and registered in stack for navigation

--- App.js
+++ App.js
@@ -3,16 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MealOverviewScreen from './screens/MealOverviewScreen';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
     return (
         <>
-            <StatusBar style='light' />
+            <StatusBar style='dark' />
             <NavigationContainer>
                 <Stack.Navigator>
                     <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+                    <Stack.Screen name="MealOverview" component={MealOverviewScreen} />
                 </Stack.Navigator>
             </NavigationContainer>
         </>
---
+++ screens/MealOverviewScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MealOverviewScreen() {
+    return (
+        <View>
+            <Text>Meal Overview</Text>
+        </View>
+    )
+}
