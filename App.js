8d4dea41efcae196e7af1bd48c4b36a2908decd7
praneeth <gp.gunturu@gmail.com>
2025-07-01 12:04:34 -0500

    displaying the meals categories

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Meals</Text>
+      <CategoriesScreen />
       <StatusBar style="auto" />
     </View>
   );
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,4 +1,4 @@
-import { FlatList } from "react-native";
+import { FlatList, Text } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 
 export default function CategoriesScreen() {
@@ -6,7 +6,7 @@ export default function CategoriesScreen() {
         <FlatList 
             data={CATEGORIES} 
             keyExtractor={(item) => item.id}    
-            renderItem={(itemData) => itemData.item.title}
+            renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
         />
     )
 }
