587470848a261622ec3964a943cf323a28484617
praneeth <gp.gunturu@gmail.com>
2025-07-02 15:59:21 -0500

    more styles

--- components/MealItem.js
+++ components/MealItem.js
@@ -4,16 +4,16 @@ import { View } from "react-native";
 
 export default function MealItem({title, imageUrl, affordability, complexity, duration}) {
     return (
-        <View>
+        <View style={styles.mealItem} >
             <Pressable>
                 <View>
                     <Image source={{uri: imageUrl}} style={styles.image} />
                     <Text style={styles.title} >{title}</Text>
                 </View>
-                <View>
-                    <Text>{duration}</Text>
-                    <Text>{complexity.toUpperCase()}</Text>
-                    <Text>{affordability.toUpperCase()}</Text>
+                <View style={styles.details} >
+                    <Text style={styles.detailItem} >{duration}</Text>
+                    <Text style={styles.detailItem} >{complexity.toUpperCase()}</Text>
+                    <Text style={styles.detailItem} >{affordability.toUpperCase()}</Text>
                 </View>
             </Pressable>
         </View>
@@ -21,6 +21,17 @@ export default function MealItem({title, imageUrl, affordability, complexity, du
 }
 
 const styles = StyleSheet.create({
+    mealItem: {
+        margin: 16,
+        borderRadius: 16,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 8,
+    },
     image: {
         width: '100%',
         height: 200
@@ -28,6 +39,17 @@ const styles = StyleSheet.create({
     title: {
         fontWeight: 'bold',
         textAlign: 'center',
-        fontSize: 18
+        fontSize: 18,
+        margin: 8
+    },
+    details: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8
+    },
+    detailItem: {
+        marginHorizontal: 4,
+        fontSize: 12
     }
 })
