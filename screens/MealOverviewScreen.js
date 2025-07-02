45a33a3e0061292245a958a87007df345e6e1eb8
praneeth <gp.gunturu@gmail.com>
2025-07-02 15:07:21 -0500

    filtering the meals via category wise and displayin using flatlist

--- screens/MealOverviewScreen.js
+++ screens/MealOverviewScreen.js
@@ -1,11 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
 export default function MealOverviewScreen({route}) {
     const categoryId = route.params.categoryId
 
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) >= 0)
+
     return (
         <View>
-            <Text>Meal Overview - {categoryId}</Text>
+            <FlatList 
+                data={displayedMeals}
+                keyExtractor={item => item.id} 
+                renderItem={itemData => itemData.item.title}
+            />
         </View>
     )
 }
