62071c8f77a0949d46cf8d61f32b5469b32661d6
praneeth <gp.gunturu@gmail.com>
2025-07-02 15:40:55 -0500

    adding styles

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,3 +1,4 @@
+import { StyleSheet } from "react-native";
 import { Image, Pressable, Text } from "react-native";
 import { View } from "react-native";
 
@@ -6,10 +7,22 @@ export default function MealItem({title, imageUrl}) {
         <View>
             <Pressable>
                 <View>
-                    <Image source={{uri: imageUrl}} />
-                    <Text>{title}</Text>
+                    <Image source={{uri: imageUrl}} style={styles.image} />
+                    <Text style={styles.title} >{title}</Text>
                 </View>
             </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18
+    }
+})
