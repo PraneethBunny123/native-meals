f24eb6bbe438946a26fd0ff9cb6e00fbdc6daefb
praneeth <gp.gunturu@gmail.com>
2025-07-02 15:13:24 -0500

    outsouced the meal item into new component

--- screens/MealOverviewScreen.js
+++ screens/MealOverviewScreen.js
@@ -1,17 +1,24 @@
 import { View, Text, FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 export default function MealOverviewScreen({route}) {
     const categoryId = route.params.categoryId
 
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) >= 0)
 
+    function mealOverviewItem(itemData) {
+        return (
+            <MealItem title={itemData.item.title} />
+        )
+    }
+
     return (
         <View>
             <FlatList 
                 data={displayedMeals}
                 keyExtractor={item => item.id} 
-                renderItem={itemData => itemData.item.title}
+                renderItem={mealOverviewItem}
             />
         </View>
     )
