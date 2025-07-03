074502558af1424aa62b21506ba004c316ff567c
praneeth <gp.gunturu@gmail.com>
2025-07-03 18:09:36 -0500

    more props

--- components/Icon.js
+++ components/Icon.js
@@ -1,10 +1,19 @@
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import {Ionicons} from '@expo/vector-icons'
 
-export default function IconButton({handleIconPress}) {
+export default function IconButton({handleIconPress, icon, color}) {
     return (
-        <Pressable onPress={handleIconPress} >
-            <Ionicons name="star" size={24} color='white' />
+        <Pressable 
+            onPress={handleIconPress} 
+            style={({pressed}) => pressed && StyleSheet.pressed}    
+        >
+            <Ionicons name={icon} size={24} color={color} />
         </Pressable>
     )
 }
+
+const styles = StyleSheet.create({
+    pressed: {
+        opacity: 0.7
+    }
+})
