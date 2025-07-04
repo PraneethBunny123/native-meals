4f9b1a4a4c6d677eb88caf1ba54dcce6158906a7
praneeth <gp.gunturu@gmail.com>
2025-07-04 14:27:18 -0500

    Nesting Navigators. Nested Drawer inside the Stack

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MealOverviewScreen from './screens/MealOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
+function DrawerNavigator() {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name='categories' component={CategoriesScreen} />
+        </Drawer.Navigator>
+    )
+}
 
 export default function App() {
     return (
@@ -23,7 +33,7 @@ export default function App() {
                 >
                     <Stack.Screen 
                         name="MealsCategories" 
-                        component={CategoriesScreen} 
+                        component={DrawerNavigator} 
                         options={{
                             title: 'All Categories',
                         }}    
